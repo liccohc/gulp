10eae3bb74dcf4eb78bb3ebf18df852caef0afe3
liccohc <155593419+liccohc@users.noreply.github.com>
2024-06-13 21:11:09 -0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const sass = require('gulp-sass')(require('sass'));
+
+const uglify = require('gulp-uglify');
+
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens(){
+    return gulp.src('/source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass(){
+    return gulp.src('./source/styles/*main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('.build/styles'));
+}
+
+exports.sass = compilaSass;
+exports.javascript = comprimeJavaScript;
+exports.images = comprimeImagens;
